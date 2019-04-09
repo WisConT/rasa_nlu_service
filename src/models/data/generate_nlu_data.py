f3a26e788762d66184163563da234e89182a838a
Jason Tsai <piniaplai@Jasons-MacBook-Pro-2.local>
2019-04-09 21:44:07 +0800

    [JT] Generate (wnut) nlu data from test.txt and perform cross validation giving average error

---
+++ src/models/data/generate_nlu_data.py
@@ -0,0 +1,28 @@
+import sys
+import os
+import json
+
+dirname = os.path.dirname(__file__)  # NOQA: E402
+filename = os.path.join(dirname, '../../')  # NOQA: E402
+sys.path.append(filename)  # NOQA: E402
+
+from data.make_dataset_wnut import parse_file
+
+documents = parse_file(os.path.join(
+    dirname, '../../../data/external/wnut_2017/test.txt'))
+
+nlu_data = {"rasa_nlu_data": {
+    "common_examples": [],
+    "regex_features": [],
+    "lookup_tables": [],
+    "entity_synonyms": []
+}}
+#print(json.dumps(documents[0:2], separators=(',', ':'), indent=4))
+common_examples = [{"intent": "greeting", "entities": doc["entities"], "text": doc["full_text"]}
+                   for [doc] in documents]
+nlu_data["rasa_nlu_data"]["common_examples"] = common_examples
+
+# print(json.dumps(nlu_data, separators=(',', ':'), indent=4))
+
+with open(dirname + '/wnut-nlu-data.json', 'w') as outfile:
+    json.dump(nlu_data, outfile, separators=(',', ':'), indent=4)
